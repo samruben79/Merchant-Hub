ecab0b9186a049d56dcfc66900b4325d6046d0d6
Ruben <sruben2@conversantmedia.com>
2018-10-24 13:10:50 -0500

    added prefix to link

--- test.js
+++ test.js
@@ -9,28 +9,12 @@ document.getElementById("test").addEventListener('click', () => {
         var links = [];
         var tds = $('#program_directory_table').find('a').each(function(){
           var link = $(this).attr('href')
-          links.push(link);
+          links.push('https://merchanthub.fmtc.co'+link);
 
 
         });
         console.log(links)
-        // console.log(tds)
-        // aLink = tds.children('a');
-        // console.log('Alink:',aLink);
-        // var link = aLink.attr('href')
-        // for(var i = 0; i< aLink.length; i++){
-        //   var link = aLink.attr('href')
-        //   links.push(link);
 
-
-        // console.log(links)
-        // var links = aLink.attr('href')
-        // console.log('href',links)
-        // var aLink = tds.children('a')
-        // console.log(aLink)
-
-        // console.log(document.body);
-        // return document.body.innerHTML;
     }
 
     //We have permission to access the activeTab, so we can call chrome.tabs.executeScript:
