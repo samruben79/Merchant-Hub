21a98df4e3e81118663f23d704672180db9052ff
Ruben <sruben2@conversantmedia.com>
2018-10-24 13:07:21 -0500

    List of links working

--- test.js
+++ test.js
@@ -2,22 +2,35 @@ document.getElementById("test").addEventListener('click', () => {
     console.log("Popup DOM fully loaded and parsed");
 
     function modifyDOM() {
+
+      var links = [];
         //You can play with your DOM here or check URL against your regex
         console.log('Tab script:');
         var links = [];
-        var tds = $('#program_directory_table').children('tbody').children('tr').children('td')
-        console.log(tds)
+        var tds = $('#program_directory_table').find('a').each(function(){
+          var link = $(this).attr('href')
+          links.push(link);
+
+
+        });
+        console.log(links)
+        // console.log(tds)
+        // aLink = tds.children('a');
+        // console.log('Alink:',aLink);
+        // var link = aLink.attr('href')
+        // for(var i = 0; i< aLink.length; i++){
+        //   var link = aLink.attr('href')
+        //   links.push(link);
+
+
+        // console.log(links)
+        // var links = aLink.attr('href')
+        // console.log('href',links)
         // var aLink = tds.children('a')
         // console.log(aLink)
-        for (var i = 0; i < tds.length; i++){
-          var aLink = tds.children('a')
-          console.log(aLink);
-          var tdLink = aLink.attr("href");
-          links.push(tdLink)
-        }
-        console.log(links)
+
         // console.log(document.body);
-        return document.body.innerHTML;
+        // return document.body.innerHTML;
     }
 
     //We have permission to access the activeTab, so we can call chrome.tabs.executeScript:
